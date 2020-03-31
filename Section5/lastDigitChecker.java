9a7325cb769e71232433c8e8821104d8c0b64e10
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-03-31 11:38:28 -0600

    Update lastDigitChecker.java

--- Section5/lastDigitChecker.java
+++ Section5/lastDigitChecker.java
@@ -1,1 +1,22 @@
+public class LastDigitChecker{
 
+	public static void main(String[] args) {
+		System.out.println(hasSameLastDigit(41, 22, 71));
+	}
+
+	public static boolean hasSameLastDigit(int x, int y, int z){
+		if((x < 10 && x > 1000)||(y < 10 && y > 1000)||(z < 10 && z > 1000))
+			return false;
+
+		int a,b,c;
+
+		a = x % 10;
+		b = y % 10;
+		c = z % 10;
+
+		if((a == b)||(a == c)||(b == c))
+			return true;
+		else
+			return false;
+	} 
+}
