80a9f955be4e4e8bf6898fa3f72462adaacc0303
Traian Alex Oglinzanu <alex.traian.oglinzanu@gmail.com>
2020-03-31 10:42:40 -0600

    Update firstAndLastDigitSum.java

--- Section5/firstAndLastDigitSum.java
+++ Section5/firstAndLastDigitSum.java
@@ -1,1 +1,27 @@
+public class FirstAndLastDigitSum{
 
+	public static void main(String[] args) {
+		System.out.println(sumFirstAndLastDigit(252));
+	}
+
+	public static int sumFirstAndLastDigit(int number){
+		if(number < 0)
+			return -1;
+
+		int firstDigit, lastDigit; 
+
+		/* Last Digit */
+
+		lastDigit = number % 10;
+
+		/* First Digit */
+
+		firstDigit = number;
+
+		while(firstDigit >= 0){
+			firstDigit /= 10;
+		}
+
+		return (firstDigit + lastDigit);
+	}
+}
