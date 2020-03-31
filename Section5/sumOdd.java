6c1efc9778982e2d8aff2cfe9aa6e90fc8b69313
Traian Alex Oglinzanu <alex.traian.oglinzanu@gmail.com>
2020-03-30 20:18:34 -0600

    Update sumOdd.java

--- Section5/sumOdd.java
+++ Section5/sumOdd.java
@@ -1,1 +1,26 @@
+public class SumOdd{
 
+	public static void main(String[] args) {
+		System.out.println(sumOdd(1, 100));
+	}
+
+	public static boolean isOdd(int number){
+		if((number < 0)||(number % 2 != 0))
+			return false;
+		else
+			return true;
+	}
+
+	public static int sumOdd(int start, int end){
+		if((start < 0) || (end < 0))
+			return -1;
+
+		int sum = 0;
+
+		for(int i = start; i <= end; i++)
+			if(isOdd(i))
+				sum += i;
+		
+		return sum;	
+	}
+}
