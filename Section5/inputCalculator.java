0947ed001087000d832e28d320e04059fcaf37c1
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 14:41:02 -0600

    Update inputCalculator.java

--- Section5/inputCalculator.java
+++ Section5/inputCalculator.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
 
+public class InputCalculator{
+
+	public static void main(String[] args) {
+		inputThenPrintSumAndAverage();
+	}
+
+	public static void inputThenPrintSumAndAverage(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter numbers: ");
+		boolean hasNextInt = scanner.hasNextInt();
+
+		int sum = 0;
+		int count = 0;
+
+		while(true){
+			if(hasNextInt){
+				int number = scanner.nextInt();
+				scanner.nextLine();
+				sum += number;
+				count++;
+
+			}else{
+				System.out.println("Invalid");
+				break;
+			}
+		}
+		int average = Math.round(sum / count);
+		System.out.println("Sum = " + sum + "Average = " + average);
+		scanner.close();
+	}
+}
