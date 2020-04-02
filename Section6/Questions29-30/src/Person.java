1df114e3ecbf937ec83ad138cb3a660973b2b1f5
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-02 13:37:50 -0600

    Create Person.java

---
+++ Section6/Questions29-30/src/Person.java
@@ -0,0 +1,55 @@
+package com.example.challenge;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFullName() {
+        if(firstName.isEmpty() && lastName.isEmpty()) {
+            return null;
+        }else if(firstName.isEmpty()) {
+            return lastName;
+        }else if(lastName.isEmpty()) {
+            return firstName;
+        }else{
+            return (firstName + ' ' + lastName);
+        }
+    }
+
+    public boolean isTeen() {
+        if(age > 12 && age < 20){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if((age < 0) || (age > 100)){
+            this.age = 0;
+        }else {
+            this.age = age;
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
