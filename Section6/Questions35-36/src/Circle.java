899628a77548df0557b0e80cd479813fbaf70d16
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-07 18:30:16 -0600

    Create Circle.java

---
+++ Section6/Questions35-36/src/Circle.java
@@ -0,0 +1,23 @@
+package com.example.challenge;
+
+/* Question 35 */
+
+public class Circle {
+    private double radius;
+
+    public double getArea() {
+        return (radius*radius*Math.PI);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public Circle(double radius) {
+        if(radius < 0) {
+            this.radius = 0;
+        }else{
+            this.radius = radius;
+        }
+    }
+}
