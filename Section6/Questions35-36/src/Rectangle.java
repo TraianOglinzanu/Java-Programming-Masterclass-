af76ffbd93a56dac2f3251f60d91a025a003e505
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-07 18:49:33 -0600

    Create Rectangle.java

---
+++ Section6/Questions35-36/src/Rectangle.java
@@ -0,0 +1,33 @@
+package com.example.challenge;
+
+/*Question 36 */
+
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public double getArea() {
+        return(width * length);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public Rectangle(double width, double length) {
+        if(width < 0) {
+            this.width = 0;
+        }else {
+            this.width = width;
+        }
+        if(length < 0) {
+            this.length = 0;
+        }else {
+            this.length = length;
+        }
+    }
+}
