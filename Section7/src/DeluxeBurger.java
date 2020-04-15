ffe3c382d675c60944b98b4c25aa401433a9ba33
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-14 18:15:31 -0600

    Update DeluxeBurger.java

--- Section7/src/DeluxeBurger.java
+++ Section7/src/DeluxeBurger.java
@@ -1,1 +1,23 @@
+package com.example.oopChallenge;
+
+public class DeluxeBurger extends Hamburger {
+    private double drink;
+    private boolean chips;
+
+    @Override
+    public void order() {
+        super.order();
+    }
+
+    @Override
+    public void totalCost() {
+        super.totalCost();
+    }
+
+    public DeluxeBurger(String breadRoll, String meat, double drink, boolean chips) {
+        super(breadRoll, meat);
+        this.drink = drink;
+        this.chips = chips;
+    }
+}
 
